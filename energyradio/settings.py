29b7dfe430ec51d16c131d2094e9b6546092c0c6
jules200 <muhayejules@gmail.com>
2026-02-14 19:15:12 +0200

    first

--- energyradio/settings.py
+++ energyradio/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = 'django-insecure-a87jmq)4vx7oqtx&6xzdy1b-j(-ae^de-@c3(lvi2%!8n*k*62
 DEBUG = True
 
 if DEBUG is False:
-    ALLOWED_HOSTS = ['44.207.175.39']
-    HOST = "http://44.207.175.39/"
+    ALLOWED_HOSTS = ['44.207.175.39', 'energy.rw', 'www.energy.rw']
+    HOST = "https://energy.rw"
 else:
     ALLOWED_HOSTS = ['*']
     HOST = "http://127.0.0.1:8000/"
@@ -89,7 +89,6 @@ if DEBUG is False:
             'NAME': 'energyradio',
             'USER': 'energy',
             'PORT': '3306',
-            'HOST': '127.0.0.1',
             'PASSWORD': 'Energy@radio12',
             'OPTIONS': {  
                 'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
