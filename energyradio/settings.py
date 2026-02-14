049d986fb9738e672af73c1eede44d75f79b72e3
jules200 <muhayejules@gmail.com>
2026-02-14 22:33:15 +0200

    first

--- energyradio/settings.py
+++ energyradio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a87jmq)4vx7oqtx&6xzdy1b-j(-ae^de-@c3(lvi2%!8n*k*62'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 if DEBUG is False:
     ALLOWED_HOSTS = ['44.207.175.39', 'energy.rw', 'www.energy.rw']
